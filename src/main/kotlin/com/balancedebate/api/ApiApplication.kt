75853b20c6b1cd878ceee33c748f8438fa227e6f
YooInKeun <keun0390@naver.com>
2025-06-15 20:18:24 +0900

    add initial project

---
+++ src/main/kotlin/com/balancedebate/api/ApiApplication.kt
@@ -0,0 +1,12 @@
+package com.balancedebate.api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.context.annotation.ComponentScan
+
+@SpringBootApplication
+class ApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<ApiApplication>(*args)
+}
